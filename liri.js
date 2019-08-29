5f93cabe53677dd11a644a35ac2c100574e979e6
Johnathan Mundt <johnathanmg1478@gmail.com>
2019-08-28 19:10:58 -0700

    forgot to save var

---
+++ liri.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+var keys = require("./keys.js");
+var axios = require("axios");
+var Spotify = require('node-spotify-api');
+var moment = require("moment");
+var fs = require("fs");
