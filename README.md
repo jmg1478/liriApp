01484db4e51544b9bde083ddb69e9f35ea7f109d
Johnathan Mundt <johnathanmg1478@gmail.com>
2019-08-30 15:46:18 -0700

    all done

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # liriApp
+This app is to find the infomation on artist, movies and concerts with out the use of google. It is pulling all the information requested in an easy flormat to view.
+
+The app is orginized in sections for each var for easy understanding. When you need to access certian information just go to that part and all the information for concerts, movies or artist are there.
+
+To use please enter (node liri[concert-this, movies-this, or sportify-this song]) + what one of the types you are trying to find information on. once enter is hit the bash will pull all the information on that subject. 
+
+Technologies used are as listed{
+    axios,
+    node-spotify-api
+    moment
+    fs
+    npm for bandsintown, ombi and spotify
+    api keys for spotify and obmi
+}
+my role in this app was main developer. 
--- liri.js
+++ liri.js
@@ -5,3 +5,121 @@ var axios = require("axios");
 var Spotify = require('node-spotify-api');
 var moment = require("moment");
 var fs = require("fs");
+var spotify = new Spotify(keys.spotify);
+var command = process.argv[2];
+var value = process.argv[3];
+
+
+
+switch (command) {
+    case "concert-this":
+        concertThis(value);
+        break;
+    case "spotify-this-song":
+        spotifySong(value);
+        break;
+    case "movie-this":
+        movieThis(value);
+        break;
+    case "do-what-it-says":
+        doThis(value);
+        break;
+};
+
+function concertThis(value) {
+    axios.get("https://rest.bandsintown.com/artists/" + value + "/events?app_id=codingbootcamp")
+    .then(function(response) {    
+        for (var i = 0; i < response.data.length; i++) {
+
+            var datetime = response.data[i].datetime; 
+
+            var concertResults = 
+                "\n--------------------------------------------------------------------" +
+                    "\n Venue Name: " + response.data[i].venue.name + 
+                    "\n Venue Location: " + response.data[i].venue.city +
+                    "\n Date of the Event: " + moment(datetime).format("MM-DD-YYYY");
+            console.log(concertResults);
+            appendLog(concertResults);
+        }
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+        
+
+}
+
+function spotifySong(value) {
+    if(!value){
+        value = "moonshiner";
+    }
+    spotify
+    .search({ type: 'track', query: value })
+    .then(function(response) {
+        for (var i = 0; i < 5; i++) {
+            var spotifyResults = 
+                "--------------------------------------------------------------------" +
+                    "\n Artist(s): " + response.tracks.items[i].artists[0].name + 
+                    "\n Song Name: " + response.tracks.items[i].name +
+                    "\n Album Name: " + response.tracks.items[i].album.name +
+                    "\n Preview Link: " + response.tracks.items[i].preview_url;
+                    
+            console.log(spotifyResults);
+            appendLog(spotifyResults);
+        }
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
+}
+
+function movieThis(value) {
+    if(!value){
+        value = "young guns";
+    }
+    // 
+    axios.get("https://www.omdbapi.com/?i=" + value + "&apikey=af70a92a")
+    .then(function(response) {
+        // loop for tomatoes rating
+                
+
+            var movieResults = 
+                "--------------------------------------------------------------------" +
+                    "\nMovie Title: " + response.data.Title + 
+                    "\nYear of Release: " + response.data.Year +
+                    "\nIMDB Rating: " + response.data.imdbRating +
+                    // "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value +
+                    "\nCountry Produced: " + response.data.Country +
+                    "\nLanguage: " + response.data.Language +
+                    "\nPlot: " + response.data.Plot +
+                    "\nActors/Actresses: " + response.data.Actors;
+            console.log(movieResults);
+            appendLog(movieResults);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+
+
+    
+}
+
+
+function doThis(value) {
+
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        var dataArr = data.split(',');
+        spotifySong(dataArr);
+    })
+}
+ function appendLog(text){
+    fs.appendFile("log.txt", text, function(err) {
+        if (err) {
+          console.log(err);
+        }
+      });
+
+ }
